982e5fec5928f50680fd7e26e2e9762230618819
Yannick Carer <yannick@yannick-vostro.(none)>
2013-02-21 21:10:58 +0100

    Export the most recent Endomondo workouts as TCX files.

---
+++ endomondo.py
@@ -0,0 +1,242 @@
+#!/usr/bin/env python
+
+import tcx
+import requests
+import uuid
+import socket
+import datetime
+import sys
+
+def to_datetime(v):
+	return datetime.datetime.strptime(v, "%Y-%m-%d %H:%M:%S %Z")
+
+def to_float(v):
+	if v == '' or v == None:
+		return None
+	return float(v)
+
+def to_int(v):
+	if v == '' or v == None:
+		return None
+	return float(v)
+
+def to_meters(v):
+	v = to_float(v)
+	if v:
+		v *= 1000
+	return v
+
+SPORTS = {
+	0:	'Running',
+	1:	'Cycling, transport',
+	2:	'Cycling, sport',
+	3:	'Mountain biking',
+	4:	'Skating',
+	5:	'Roller skiing',
+	6:	'Skiing, cross country',
+	7:	'Skiing, downhill',
+	8:	'Snowboarding',
+	9:	'Kayaking',
+	10:	'Kite surfing',
+	11:	'Rowing',
+	12:	'Sailing',
+	13:	'Windsurfing',
+	14:	'Fitness walking',
+	15:	'Golfing',
+	16:	'Hiking',
+	17:	'Orienteering',
+	18:	'Walking',
+	19:	'Riding',
+	20:	'Swimming',
+	21:	'Spinning',
+	22:	'Other',
+	23:	'Aerobics',
+	24:	'Badminton',
+	25:	'Baseball',
+	26:	'Basketball',
+	27:	'Boxing',
+	28:	'Climbing stairs',
+	29:	'Cricket',
+	30:	'Cross training',
+	31:	'Dancing',
+	32:	'Fencing',
+	33:	'Football, American',
+	34:	'Football, rugby',
+	35:	'Football, soccer',
+	36:	'Handball',
+	37:	'Hockey',
+	38:	'Pilates',
+	39:	'Polo',
+	40:	'Scuba diving',
+	41:	'Squash',
+	42:	'Table tennis',
+	43:	'Tennis',
+	44:	'Volleyball, beach',
+	45:	'Volleyball, indoor',
+	46:	'Weight training',
+	47:	'Yoga',
+	48:	'Martial arts',
+	49:	'Gymnastics',
+	50:	'Step counter'
+	}
+
+class Endomondo:
+	os = "Android"
+	os_version = "2.2"
+	model = "M"
+
+	def __init__(self, email = None, password = None):
+		self.auth_token = None
+		self.request = requests.session()
+		self.request.headers['User-Agent'] = self.get_user_agent()
+		if email and password:
+			self.auth_token = self.request_auth_token(email, password)
+
+	def get_user_agent(self):
+		"""HTTP User-Agent"""
+		return "Dalvik/1.4.0 (Linux; U; %s %s; %s Build/GRI54)" % (self.os, self.os_version, self.model)
+
+	def get_device_id(self):
+		return str(uuid.uuid5(uuid.NAMESPACE_DNS, socket.gethostname()))
+
+	def request_auth_token(self, email, password):
+
+		# API request for authentication
+		params = {
+			'email':            email,
+			'password':			password,
+			'country':          'US',
+			'deviceId':         self.get_device_id(),
+			'os':				self.os,
+			'appVersion':		"7.1",
+			'appVariant':		"M-Pro",
+			'osVersion':		self.os_version,
+			'model':			self.model,
+			'v':				2.4,
+			'action':			'PAIR'
+			}
+		r = self.request.get('http://api.mobile.endomondo.com/mobile/auth',
+							 params = params)
+		lines = r.text.split("\n")
+
+		# check success
+		if lines[0] != "OK":
+			raise ValueError("Endomondo authentication failed, %s" % lines[0])
+
+		# retrieve auth token
+		for line in lines[1:]:
+			key, value = line.split("=")
+			if key == "authToken":
+				return value
+		
+		return None
+
+	def parse_text(self, response):
+		"""Parse API response as text"""
+		lines = response.text.split("\n")
+		if (len(lines) < 1):
+			raise ValueError("Error: URL %s: empty response" % response.url)
+
+		if lines[0] != "OK":
+			raise ValueError("Error: URL %s: %s" % (response.url, lines[0]))
+		return lines[1:]
+
+	def parse_json(self, response):
+		"""Parse API response as JSON"""
+		return response.json()['data']		
+
+	def call(self, url, format, params={}):
+		"""Call the Endomondo API"""
+
+		# API request
+		params.update({ 'authToken': self.auth_token,
+                        'language': 'EN'
+						})
+		r = self.request.get('http://api.mobile.endomondo.com/mobile/' + url,
+							 params = params)
+
+		# check success
+		if r.status_code != requests.codes.ok:
+			print "Error: failed GET URL %s" % r.url
+			r.raise_for_status()
+			return None
+		
+	  	# parse response in the appropriate format
+		if format == 'text':
+			return self.parse_text(r)
+		if format == 'json':
+			return self.parse_json(r)
+		return r
+
+	def get_workouts(self, max_results = 40):
+		"""Get the most recent workouts"""
+		if not max_results:
+			max_results = 100000000
+		json = self.call('api/workout/list', 'json', 
+						 { 'maxResults' : max_results })
+		return [ EndomondoWorkout(self, w) for w in json]
+
+class EndomondoWorkout:
+	"""Endomondo Workout wrapper"""
+
+	def __init__(self, parent, properties):
+		self.parent = parent
+		self.properties = properties
+		self.activity = None
+
+	# dict wrapper
+	def __getattr__(self, name):
+		value = None
+		if self.properties.has_key(name):
+			value = self.properties[name]
+			if name == 'sport':
+				value = SPORTS.get(value, 'Other')
+			elif name == 'start_time':
+				value = to_datetime(value)
+		return value
+
+	def get_activity(self):
+		"""The TCX activity equivalent to this workout""" 
+
+		if self.activity:
+			return self.activity
+
+		# call to retrieve activity data
+		lines = self.parent.call('readTrack', 'text', {'trackId': self.id})
+
+		# the 1st line is activity details
+		data = lines[0].split(";")
+		start_time = to_datetime(data[6])
+		total_distance_meters = to_meters(data[8])
+		self.activity = tcx.Activity()
+		self.activity.sport = SPORTS.get(int(data[5]), "Other")
+		self.activity.start_time = start_time
+		self.activity.notes = self.notes
+			
+		# create a single lap for the whole activity
+		l = tcx.ActivityLap()
+		l.start_time = start_time
+		l.timestamp = start_time
+		l.total_time_seconds = to_float(data[7])
+		l.distance_meters = to_meters(data[8])
+		l.calories = to_int(data[9])
+		l.min_altitude = to_float(data[11])
+		l.max_altitude = to_float(data[12])
+		l.max_heart = to_float(data[13])
+		l.avg_heart = to_float(data[14])
+		self.activity.laps.append(l)
+
+		# extra lines are activity trackpoints
+		for line in lines[1:]:
+			data = line.split(";")
+			if len(data) >= 9:
+				w = tcx.Trackpoint()
+				w.timestamp = to_datetime(data[0])
+				w.latitude = to_float(data[2])
+				w.longitude = to_float(data[3])
+				w.altitude_meters = to_float(data[6])
+				w.distance_meters = to_meters(data[4])
+				w.heart_rate = to_int(data[7])
+				self.activity.trackpoints.append(w)
+					
+	   	return self.activity
---
+++ tcx.py
@@ -0,0 +1,139 @@
+import lxml.etree
+import datetime
+import time
+
+class Trackpoint:
+    def __init__(self):
+		self.timestamp = None
+		self.latitude = 0.0
+		self.longitude = 0.0
+		self.altitude_meters = 0.0
+		self.distance_meters = 0.0
+		self.sensor_state = "Absent"
+		self.heart_rate = None
+		self.cadence = None
+
+class ActivityLap:
+    def __init__(self):
+		self.start_time = None
+		self.timestamp = None
+		self.total_time_seconds = 0.0
+		self.distance_meters = 0.0
+		self.maximum_speed = 0.0
+		self.calories = 0
+		self.intensity = "Active"
+		self.trigger_method = "Distance"
+		self.avg_heart = 0.0
+		self.max_heart = 0.0
+		self.avg_cadence = 0.0
+		self.cadence = None
+
+class Activity:    
+    def __init__(self):
+		self.sport = "Running"
+		self.start_time = ''
+		self.notes = None
+		self.laps = []
+		self.trackpoints = []
+
+class Writer:
+
+    TCD_NAMESPACE = "http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2"
+    XML_SCHEMA_NAMESPACE = "http://www.w3.org/2001/XMLSchema-instance"
+
+    def create_element(self, parent, tag, text = None):
+        tag = "{%s}%s" % (self.TCD_NAMESPACE, tag)
+
+        nsmap = { 
+			None : self.TCD_NAMESPACE,
+			"xsi": self.XML_SCHEMA_NAMESPACE,
+			}
+        element = lxml.etree.Element(tag, nsmap=nsmap)
+    
+        if text:
+            element.text = text
+
+        if parent is not None:
+            parent.append(element)
+
+        return element
+
+    def time_to_s(self, t):
+        return t.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+    def add_property(self, element, name, value):
+        if value:
+            if not isinstance(value, str):
+                value = str(value)
+            self.create_element(element, name, value)
+
+    def add_trackpoint(self, element, v):
+        elem = self.create_element(element, "Trackpoint")
+
+        self.create_element(elem, "Time", self.time_to_s(v.timestamp))
+
+        if v.latitude and v.longitude:
+            pos = self.create_element(elem, "Position")
+            self.add_property(pos, "LatitudeDegrees", v.latitude)
+            self.add_property(pos, "LongitudeDegrees", v.longitude)
+
+        self.add_property(elem, "AltitudeMeters", v.altitude_meters)
+        self.add_property(elem, "DistanceMeters", v.distance_meters)
+        self.add_heart_rate(elem, "HeartRateBpm", v.heart_rate)
+        self.add_property(elem, "Cadence", v.cadence)
+
+    def add_heart_rate(self, element, name, value):
+        if value:
+            elem = self.create_element(element, name)
+            self.add_property(elem, "Value", int(value))
+
+    def add_lap(self, element, activity, lap):
+        elem = self.create_element(element, "Lap")
+        elem.set("StartTime", self.time_to_s(lap.start_time))
+
+        self.add_property(elem, "TotalTimeSeconds", lap.total_time_seconds)
+        self.add_property(elem, "DistanceMeters", lap.distance_meters)
+        self.add_property(elem, "MaximumSpeed", lap.maximum_speed)
+        self.add_property(elem, "Calories", lap.calories)
+        self.add_heart_rate(elem, "AverageHeartRateBpm", lap.avg_heart)
+        self.add_heart_rate(elem, "MaximumHeartRateBpm", lap.max_heart)
+        self.add_property(elem, "Intensity", lap.intensity)
+        self.add_property(elem, "Cadence", lap.cadence)
+        self.add_property(elem, "TriggerMethod", lap.trigger_method)
+
+        # Add trackpoints
+        tracks = self.create_element(elem, "Track")
+        for w in activity.trackpoints:
+            self.add_trackpoint(elem, w)
+
+    def add_activity(self, element, activity):
+        sport = activity.sport
+        if not sport: 
+            sport = "Other"
+
+        elem = self.create_element(element, "Activity")
+        elem.set("Sport", sport)
+        self.create_element(elem, "Id", self.time_to_s(activity.start_time))
+        self.create_element(elem, "Notes", activity.notes)
+
+        for lap in activity.laps:
+            self.add_lap(elem, activity, lap)
+
+    def create_document(self, activity):
+        document = self.create_element(None, "TrainingCenterDatabase")
+        document.set("{%s}schemaLocation" % self.XML_SCHEMA_NAMESPACE, "http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2 http://www.garmin.com/xmlschemas/TrainingCenterDatabasev2.xsd")
+
+        document = lxml.etree.ElementTree(document)
+        element = self.create_element(document.getroot(), "Activities")
+        self.add_activity(element, activity)
+        return document
+
+    def write(self, activity):
+        self.laps = []
+        self.trackpoints = []
+
+        document = self.create_document(activity)
+        return lxml.etree.tostring(document.getroot(),
+                                   pretty_print=True,
+                                   xml_declaration=True,
+                                   encoding="UTF-8")
